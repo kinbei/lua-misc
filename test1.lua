b281743215af9f4d2fe5182c999f3e44a5df57f9
sleepwom <sleepwom@163.com>
2015-03-19 10:31:58 +0800

    commit

--- test1.lua
+++ test1.lua
@@ -1,6 +1,3 @@
-t = {["kk"]=1}
-
-
 local function test1()
 	return 1, 2, 3
 end
@@ -13,5 +10,10 @@ local function test3(a, b, c)
 	print(a, b, c)
 end
 
+local function test4(a1, a, b, c)
+  print(a1, a, b, c)
+end
+
 test2( 11, test1(), 12, 13 ) -- > 11      1       12      13      nil     nil
 test3( test1() ) -- > 1, 2, 3
+test4( 10, test1() ) --> 10, 1, 2, 3
