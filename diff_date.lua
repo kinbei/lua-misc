51c8d862db0c6fbfd51863d00e289e6f37b4e5ca
Kin <kinbei@qq.com>
2016-05-06 19:46:27 +0800

    commit

--- diff_date.lua
+++ diff_date.lua
@@ -1,6 +1,6 @@
 -- 给出两个时间戳, 返回它们相差的天数(按自然天计算)
 local function diff_time_stamp( time_stamp_1, time_stamp_2 )
-        local t1_begin = math.floor( time_stamp_1 / ( 3600 * 24 ) )
-        local t2_begin = math.floor( time_stamp_2 / ( 3600 * 24 ) )
-        return math.abs( t1_begin - t2_begin )
+        local t1 = math.floor( time_stamp_1 / ( 3600 * 24 ) )
+        local t2 = math.floor( time_stamp_2 / ( 3600 * 24 ) )
+        return math.abs( t1 - t2 )
 end
