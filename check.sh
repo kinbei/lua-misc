e0d5cfcf882f729ba30e7978b2fdceba0a98b690
Kin <kinbei@qq.com>
2016-05-12 00:30:37 +0800

    commit

--- check.sh
+++ check.sh
@@ -1,4 +1,8 @@
 # call from .travis/setup_lua.sh
 
+check_retcode() {
+	[[ $1 -ne 0 ]] && exit 1
+}
+
 $1 -v
-find ${TRAVIS_BUILD_DIR} -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" -not -path "${TRAVIS_BUILD_DIR}/lua/*" -exec $1 {} \;
+find ${TRAVIS_BUILD_DIR} -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" -not -path "${TRAVIS_BUILD_DIR}/lua/*" -exec $1 {} && check_retcode $? \;
