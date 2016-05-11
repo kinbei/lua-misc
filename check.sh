4b92dff73f75c4eaf8f01bf457278538a36fdd38
Kin <kinbei@qq.com>
2016-05-11 23:48:34 +0800

    commit

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 # call from .travis/setup_lua.sh
 
 $1 -v
-find ${TRAVIS_BUILD_DIR} -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" | grep -v ${TRAVIS_BUILD_DIR}/lua/ | xargs lua
+find ${TRAVIS_BUILD_DIR} -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" | grep -v ${TRAVIS_BUILD_DIR}/lua/ | xargs $1
