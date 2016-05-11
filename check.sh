8bd8ea40c618a4a74b8875618a0920f4fc2e6220
Kin <kinbei@qq.com>
2016-05-11 23:36:52 +0800

    commit

--- check.sh
+++ check.sh
@@ -1,6 +1,4 @@
 # call from .travis/setup_lua.sh
 
 $1 -v
-find ./ -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" -exec $1 {} \;
-
-$1 ${TRAVIS_BUILD_DIR}/trace_log.lua
+find ${TRAVIS_BUILD_DIR} -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" -exec $1 {} \;
