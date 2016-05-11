b28b8c02805c20d24ab7878d58623b1027c95c5f
Kin <kinbei@qq.com>
2016-05-11 18:38:36 +0800

    commit

--- trace_log.lua
+++ trace_log.lua
@@ -1,12 +1,12 @@
 local function trace_log(fmt, ...)
-        local t = debug.getinfo( 2, "Sl" )
-        print( string.format("%s:%s: " .. fmt, t.source, tostring(t.currentline), ...) )
+        local t = debug.getinfo( 2, "nSl" )
+        print( string.format("%s:%s - %s: " .. fmt, t.source, tostring(t.currentline), tostring(t.name), ...) )
 end
 
-function test_trace_log() 
+function test_trace_log()
         local function abc()
                 trace_log("test log")
-        end 
+        end
         abc()
 end
 
