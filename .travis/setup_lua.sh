0ef23fafbfa5489de00835ccae6ec2aa3885773d
Kin <kinbei@qq.com>
2016-05-11 21:23:41 +0800

    commit

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -8,3 +8,11 @@ curl --location http://www.lua.org/ftp/lua-5.1.tar.gz | tar xz;
 cd ${LUA_5_1_DIR}
 sudo make linux && sudo make local
 ${LUA_5_1_DIR}/bin/lua -v
+
+# lua 5.2
+cd ${LUA_HOME_DIR}
+LUA_5_2_DIR=${LUA_HOME_DIR}/lua-5.2
+curl --location http://www.lua.org/ftp/lua-5.2.0.tar.gz | tar xz;
+cd ${LUA_5_2_DIR}
+sudo make linux && sudo make local
+${LUA_5_2_DIR}/bin/lua -v
