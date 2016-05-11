d1b2e7d302571ac8f689f5c811fa533a497c33d0
Kin <kinbei@qq.com>
2016-05-11 21:51:20 +0800

    commit

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -24,3 +24,11 @@ curl --location http://www.lua.org/ftp/lua-5.3.2.tar.gz | tar xz;
 cd ${LUA_5_3_DIR}
 sudo make linux && sudo make install INSTALL_TOP=${LUA_5_3_DIR}
 ${LUA_5_3_DIR}/bin/lua -v
+
+# lua jit
+cd ${LUA_HOME_DIR}
+LUA_JIT_DIR=${LUA_HOME_DIR}/lua-jit
+git clone https://github.com/LuaJIT/LuaJIT.git
+cd ${LUA_JIT_DIR}
+sudo make linux && sudo make install PREFIX=${LUA_5_3_DIR}
+${LUA_JIT_DIR}/bin/lua -v
