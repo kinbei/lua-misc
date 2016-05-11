0f3f23376df62f4e27b0041b2f1cf204d5601aaa
Kin <kinbei@qq.com>
2016-05-11 22:10:03 +0800

    commit

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -32,5 +32,5 @@ git clone https://github.com/LuaJIT/LuaJIT.git
 cd ${LUA_JIT_DIR}
 sudo make && sudo make install PREFIX=${LUA_JIT_DIR}
 cd ${LUA_JIT_DIR}/bin/
-ls
-${LUA_JIT_DIR}/bin/lua -v
+ll
+${LUA_JIT_DIR}/bin/luajit -v
