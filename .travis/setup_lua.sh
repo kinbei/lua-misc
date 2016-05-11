9fe16c8c67d6b35bfabbaf2e5bc100f7b7fcd04f
Kin <kinbei@qq.com>
2016-05-11 19:36:04 +0800

    commit

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -5,7 +5,7 @@ mkdir -p ${LUA_HOME_DIR}
 LUA_5_1_DIR=${LUA_HOME_DIR}/lua51
 mkdir -p ${LUA_5_1_DIR}
 cd ${LUA_5_1_DIR}
-curl --location http://www.lua.org/ftp/lua-5.1.tar.gz | tar xz -C ${LUA_5_1_DIR};
+curl --location http://www.lua.org/ftp/lua-5.1.tar.gz | tar xz -P ${LUA_5_1_DIR};
 
 ls
 make && make install PREFIX="${LUA_5_1_DIR}"
