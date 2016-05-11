e46556ad1f395c72669a698bb2f2fbf75b8aaeed
Kin <kinbei@qq.com>
2016-05-12 00:48:40 +0800

    comit

--- check.sh
+++ check.sh
@@ -1,11 +1,14 @@
 # call from .travis/setup_lua.sh
 
 check_retcode() {
-	echo "return code is $1"
-	if [ "$1" -ne "0" ];then
+	# $1 lua bin
+	# $2 lua file name
+	$1 $2
+	echo "return code is $?"
+	if [ "$?" -ne "0" ];then
 	   exit 1;
 	fi
 }
 
 $1 -v
-find ${TRAVIS_BUILD_DIR} -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" -not -path "${TRAVIS_BUILD_DIR}/lua/*" -exec $1 {} \; ; check_retcode $?
+find ${TRAVIS_BUILD_DIR} -type f -wholename "${TRAVIS_BUILD_DIR}/*.lua" -not -path "${TRAVIS_BUILD_DIR}/lua/*" -exec check_retcode $1 {} \;
--- string_split.lua
+++ string_split.lua
@@ -5,7 +5,21 @@ function string.split(str, sep)
         return fields
 end
 
+
+local t = {
+	{item_id = 123, count = 10},
+	{item_id = 456, count = 20},
+	{item_id = 789, count = 30},
+}
+
+local function test1(t)
+	local result = ""
+	for _, v in pairs(t) do
+		result = result .. tostring(v.item_id) .. ":" .. tostring(v.count) .. ";"
+	end
+	return result
+end
+
 for k, v in pairs( string.split(test1(t), ";") ) do
 	print(k, v)
 end
-
