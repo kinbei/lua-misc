eb94ce09e4d58512164bd0b51cf18a6c97f650bb
Kin <kinbei@users.noreply.github.com>
2016-05-11 13:22:35 +0800

    Update string_split.lua

--- string_split.lua
+++ string_split.lua
@@ -1,39 +1,3 @@
-local function test1(t)
-	local result = ""
-	for _, v in pairs(t) do
-		result = result .. tostring(v.item_id) .. ":" .. tostring(v.count) .. ";"
-	end
-	return result
-end
-
-local function test2(t)
-	local result = ""
-	for _, v in pairs(t) do
-                result = result .. string.format("%d:%d;", v.item_id, v.count) 
-        end
-        return result
-end
-
-local t = {
-{item_id = 123, count = 10},
-{item_id = 456, count = 20},
-{item_id = 789, count = 30},
-}
-
---[[
-real    0m3.593s
-user    0m3.588s
-sys     0m0.002s
-]]
--- for i = 1, 1000000 do test1(t) end
-
---[[
-real    0m2.679s
-user    0m2.678s
-sys     0m0.001s
-]]
--- for i = 1, 1000000 do test2(t) end
-
 function string.split(str, sep)
         local sep, fields = sep or ":", {}
         local pattern = string.format("([^%s]+)", sep)
