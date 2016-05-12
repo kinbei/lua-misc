a52a9306d048556861050c2bd2d6bc91e89257b5
Kin <kinbei@qq.com>
2016-05-12 11:06:23 +0800

    commit

--- test_nan.lua
+++ test_nan.lua
@@ -5,9 +5,9 @@ print(test1) -- inf
 print(test2) -- -nan / luajit:nan / lua5.1:-nan
 
 local function test()
-        print( 1 % 0 ) -- error attempt to perform 'n%0' / luajit:nan / lua5.1:-nan
+        print( 1 % 0 ) -- lua5.3:error attempt to perform 'n%0' / luajit:nan / lua5.1:-nan
 end
 
-local ok, err = xpcall(test, debug.traceback)
+local ok, err = xpcall(test, debug.traceback) -- it would be error in lua5.3+
 print(ok)
 print(err)
