0b88dbb3f3a1dde27ccd6c3ff022eb6e2fdb1649
Kin <kinbei@qq.com>
2016-05-13 12:44:20 +0800

    commit

--- .travis/install_luarocks.sh
+++ .travis/install_luarocks.sh
@@ -7,4 +7,4 @@ cd ${LUAROCKS_DIR}
 wget http://luarocks.org/releases/luarocks-2.3.0.tar.gz
 tar zxpf luarocks-2.3.0.tar.gz
 cd luarocks-2.3.0
-./configure; sudo make bootstrap
+./configure --with-lua; sudo make bootstrap
