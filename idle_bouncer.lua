18bc9b544e7a5fcdd5d4ac3cbaa6d56219b93bb1
kinbei <kinbei@users.noreply.github.com>
2017-08-14 20:57:20 +0800

    Update idle_bouncer.lua

--- idle_bouncer.lua
+++ idle_bouncer.lua
@@ -2,8 +2,8 @@
 
 local t = table.pack(...)
 
-local current_power = tonumber(t[1])
-local remain = tonumber(t[2])
+local current_power = tonumber(t[1]) or 1000
+local remain = tonumber(t[2]) or 1000
 
 print(string.format("current_power = %s", current_power))
 print(string.format("remain = %s", remain))
@@ -13,7 +13,7 @@ for i = 1, remain do
         total_power = total_power + current_power
         print(string.format("current_power = %s", current_power))
         current_power = current_power * 1.15
-        current_power = current_power - current_power%0.01
+        current_power = current_power - current_power % 0.01
 end
 
 print(string.format("result : total_power = %s", total_power))
