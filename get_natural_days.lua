9232c6dd23281e8d5e5ef2b696f83c5c8a5ac4af
kinbei <kinbei@users.noreply.github.com>
2017-08-17 11:36:33 +0800

    Update and rename diff_time_stamp.lua to get_natural_days.lua

--- get_natural_days.lua
+++ get_natural_days.lua
@@ -1,5 +1,5 @@
--- ¸ø³öÁ½¸öÊ±¼ä´Á, ·µ»ØËüÃÇÏà²îµÄÌìÊı(°´×ÔÈ»Ìì¼ÆËã)
-local function diff_time_stamp( time_stamp_1, time_stamp_2 )
+-- ç»™å‡ºä¸¤ä¸ªæ—¶é—´æˆ³, è¿”å›å®ƒä»¬ç›¸å·®çš„å¤©æ•°(æŒ‰è‡ªç„¶å¤©è®¡ç®—)
+local function get_natural_days( time_stamp_1, time_stamp_2 )
         local t1 = math.floor( time_stamp_1 / ( 3600 * 24 ) )
         local t2 = math.floor( time_stamp_2 / ( 3600 * 24 ) )
         return math.abs( t1 - t2 )
