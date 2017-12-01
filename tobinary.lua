d197277d9e95620b97feb36ae0625d9485460986
kinbei <kinbei@users.noreply.github.com>
2017-12-01 12:11:56 +0800

    Create tobinary.lua

---
+++ tobinary.lua
@@ -0,0 +1,12 @@
+local function toBits(num)
+    -- returns a table of bits, least significant first.
+    local t={} -- will contain the bits
+    while num>0 do
+        rest=math.fmod(num,2)
+        t[#t+1]=rest
+        num=(num-rest)/2
+    end
+    return t
+end
+
+print( table.concat(toBits(10)) )
