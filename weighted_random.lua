40157bf9bfeed19734c0e2c37b5a7f7cc568cea3
kinbei <kinbei@qq.com>
2019-01-22 09:18:14 +0800

    优化权重随机算法

--- weighted_random.lua
+++ weighted_random.lua
@@ -6,29 +6,27 @@ local function weighted_random( tbl_weight, random_func, id, weight )
 	random_func = random_func or math.random
 	id = id or "id"
 	weight = weight or "weight"
-
+	
 	local t = {}
+	local total_index = #tbl_weight
 	local total_weight = 0
 
 	for _, v in pairs(tbl_weight) do
 		assert( type(v[weight]) == "number" )
+
 		total_weight = total_weight + v[weight]
 		table.insert(t, { [id] = v[id], [weight] = v[weight] })
 	end
-	table.sort(t, function (a, b) return a[weight] > b[weight] end)
 
 	return function()
-		local rand = random_func(1, total_weight)
-		local curweight = 0
-
-		for _, v in ipairs(t) do
-			curweight = curweight + v[weight]
-			if rand <= curweight then
-				return v[id]
+		local rand_index
+		
+		while true do
+			rand_index = random_func(1, total_index)
+			if random_func(1, total_weight) < t[rand_index].weight then
+				return t[rand_index][id]
 			end
 		end
-
-		assert(false, string.format("%d %d", rand, curweight))
 	end
 end
 
@@ -36,16 +34,31 @@ end
 math.randomseed( os.time() )
 
 local t = {}
-t[1] = { weight = 2000, id = 1 }
-t[2] = { weight = 2000, id = 2 }
-t[3] = { weight = 30, id = 3 }
-t[4] = { weight = 40, id = 4 }
-t[5] = { weight = 50, id = 5 }
+table.insert(t, { id = 1, weight = 400 } )
+table.insert(t, { id = 2, weight = 30  } )
+table.insert(t, { id = 3, weight = 100 } )
+table.insert(t, { id = 4, weight = 8   } )
+table.insert(t, { id = 5, weight = 30  } )
+table.insert(t, { id = 6, weight = 500 } )
+table.insert(t, { id = 7, weight = 20  } )
+table.insert(t, { id = 8, weight = 200 } )
+table.insert(t, { id = 9, weight = 40  } )
+table.insert(t, { id = 10, weight = 70  } )
+table.insert(t, { id = 11, weight = 300 } )
+table.insert(t, { id = 12, weight = 500 } )
+table.insert(t, { id = 13, weight = 350 } )
+table.insert(t, { id = 14, weight = 20  } )
+table.insert(t, { id = 15, weight = 480 } )
+table.insert(t, { id = 16, weight = 250 } )
+table.insert(t, { id = 17, weight = 500 } )
+table.insert(t, { id = 18, weight = 50  } )
+table.insert(t, { id = 19, weight = 300 } )
+table.insert(t, { id = 20, weight = 500 } )
 
 local f = weighted_random(t)
 
 local result = {}
-for i = 1, 1500 do
+for i = 1, 4648 do
 	local id = f()
 	result[id] = result[id] or 0
 	result[id] = result[id] + 1
