a870472383a9ee67853cf59cb46f67fcb6dea6f6
kinbei <kinbei@qq.com>
2019-01-22 09:26:15 +0800

    优化权重随机算法

--- weighted_random.lua
+++ weighted_random.lua
@@ -33,43 +33,82 @@ end
 ----------------------------------------------------------------------------------------
 math.randomseed( os.time() )
 
-local t = {}
-table.insert(t, { id = 1, weight = 400 } )
-table.insert(t, { id = 2, weight = 30  } )
-table.insert(t, { id = 3, weight = 100 } )
-table.insert(t, { id = 4, weight = 8   } )
-table.insert(t, { id = 5, weight = 30  } )
-table.insert(t, { id = 6, weight = 500 } )
-table.insert(t, { id = 7, weight = 20  } )
-table.insert(t, { id = 8, weight = 200 } )
-table.insert(t, { id = 9, weight = 40  } )
-table.insert(t, { id = 10, weight = 70  } )
-table.insert(t, { id = 11, weight = 300 } )
-table.insert(t, { id = 12, weight = 500 } )
-table.insert(t, { id = 13, weight = 350 } )
-table.insert(t, { id = 14, weight = 20  } )
-table.insert(t, { id = 15, weight = 480 } )
-table.insert(t, { id = 16, weight = 250 } )
-table.insert(t, { id = 17, weight = 500 } )
-table.insert(t, { id = 18, weight = 50  } )
-table.insert(t, { id = 19, weight = 300 } )
-table.insert(t, { id = 20, weight = 500 } )
+local function test_case_1()
+	print("test_case_1")
 
-local f = weighted_random(t)
+	local t = {}
+	table.insert(t, { id = 1, weight = 400 } )
+	table.insert(t, { id = 2, weight = 30  } )
+	table.insert(t, { id = 3, weight = 100 } )
+	table.insert(t, { id = 4, weight = 8   } )
+	table.insert(t, { id = 5, weight = 30  } )
+	table.insert(t, { id = 6, weight = 500 } )
+	table.insert(t, { id = 7, weight = 20  } )
+	table.insert(t, { id = 8, weight = 200 } )
+	table.insert(t, { id = 9, weight = 40  } )
+	table.insert(t, { id = 10, weight = 70  } )
+	table.insert(t, { id = 11, weight = 300 } )
+	table.insert(t, { id = 12, weight = 500 } )
+	table.insert(t, { id = 13, weight = 350 } )
+	table.insert(t, { id = 14, weight = 20  } )
+	table.insert(t, { id = 15, weight = 480 } )
+	table.insert(t, { id = 16, weight = 250 } )
+	table.insert(t, { id = 17, weight = 500 } )
+	table.insert(t, { id = 18, weight = 50  } )
+	table.insert(t, { id = 19, weight = 300 } )
+	table.insert(t, { id = 20, weight = 500 } )
 
-local result = {}
-for i = 1, 4648 do
-	local id = f()
-	result[id] = result[id] or 0
-	result[id] = result[id] + 1
+	local f = weighted_random(t)
+
+	local result = {}
+	for i = 1, 4648 do
+		local id = f()
+		result[id] = result[id] or 0
+		result[id] = result[id] + 1
+	end
+
+	local r = {}
+	for k, v in pairs(result) do
+		table.insert(r, { id = k, times = v })
+	end
+
+	table.sort(r, function(a, b) return a.times > b.times end)
+	for _, v in ipairs(r) do
+		print(v.id, v.times)
+	end
 end
 
-local r = {}
-for k, v in pairs(result) do
-	table.insert(r, { id = k, times = v })
+local function test_case_2()
+	print("test_case_2")
+
+	local t = {}
+	t[1] = { weight = 2000, id = 1 }
+	t[2] = { weight = 2000, id = 2 }
+	t[3] = { weight = 30, id = 3 }	
+	t[4] = { weight = 40, id = 4 }	
+	t[5] = { weight = 50, id = 5 }  
+	
+	
+        local f = weighted_random(t)
+
+        local result = {}
+        for i = 1, 4120 do
+                local id = f()
+                result[id] = result[id] or 0
+                result[id] = result[id] + 1
+        end
+
+        local r = {}
+        for k, v in pairs(result) do
+                table.insert(r, { id = k, times = v })
+        end
+
+        table.sort(r, function(a, b) return a.times > b.times end)
+        for _, v in ipairs(r) do
+                print(v.id, v.times)
+        end
 end
 
-table.sort(r, function(a, b) return a.times > b.times end)
-for _, v in ipairs(r) do
-	print(v.id, v.times)
-end
+test_case_1()
+test_case_2()
+
