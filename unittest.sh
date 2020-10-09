45087787d24d002e3e20838856fbaad8cb2b0cbb
kinbei <kinbei@qq.com>
2020-10-09 21:56:14 +0800

    install luacov

--- unittest.sh
+++ unittest.sh
@@ -6,7 +6,7 @@ WORKSPACE=$2
 chk_ret() {
 	LUA_BIN=$1
 	LUA_FILE=$2
-
+	
 	${LUA_BIN} ${LUA_FILE}
 	retcode=$?
 	if [ "${retcode}" -ne "0" ];then
@@ -18,7 +18,7 @@ chk_ret() {
 # show the lua version
 ${LUA_BIN} -v
 
-for LUA_FILE in $( find ${WORKSPACE} -type f -wholename "${WORKSPACE}/*.lua" -not -path "${WORKSPACE}/lua/*" )
+for LUA_FILE in $(find ${WORKSPACE} -type f -wholename "${WORKSPACE}/*.lua" -maxdepth 1)
 do
 	chk_ret ${LUA_BIN} ${LUA_FILE}
 done
