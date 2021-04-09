d4d0fb45df4ded4939cb35e9b8edc7d391f79b01
kinbei <kinbei@users.noreply.github.com>
2021-04-09 17:59:06 +0800

    Update queue.lua

--- queue.lua
+++ queue.lua
@@ -1,19 +1,48 @@
-q = { head = 1, tail = 1 }
-
-function push_queue(v)
-  q[q.tail] = v
-  q.tail = q.tail + 1
+local function push(self, v)
+	self[self.tail] = v
+	self.tail = self.tail + 1
 end
 
-function pop_queue()
-  if q.head == q.tail then
-    q.head = 1
-    q.tail = 1
-    return nil
-  else
-    local r = q[q.head]
-    q[q.head] = nil
-    q.head = q.head + 1
-    return r
-  end
+local function pop(self)
+	if self.head == self.tail then
+		self.head = 1
+		self.tail = 1
+		return nil
+	else
+		local r = self[self.head]
+		self[self.head] = nil
+		self.head = self.head + 1
+		return r
+	end
 end
+
+local function size(self)
+	if self.tail - self.head <= 0 then
+		return 0
+	else
+		return self.tail - self.head
+	end
+end
+
+local function create()
+	local q = {head = 1, tail = 1}
+	q.push = push
+	q.pop = pop
+	q.size = size
+	return q
+end
+return create
+
+--[[
+local q = require("queue")()
+
+q:push(1)
+q:push(2)
+q:push(3)
+
+assert(q:size() == 3)
+
+assert(q:pop() == 1)
+assert(q:pop() == 2)
+assert(q:pop() == 3)
+--]]
